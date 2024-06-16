c15c15bdabe1a96c4bcb6223ee36ebb7dd781535
Barbara Martins 1 semestre cc <bmartinsaraujoruiz@gmail.com>
2024-06-16 20:44:02 +0000

    finalizar

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -145,3 +145,26 @@ function validarResposta(){
 
     pergunta = pergunta + 1
 }
+
+function finalizar() {
+    localStorage.setItem("pontos", pontos)
+
+    window.location.href= "../resultado/resultado.html"
+}
+
+function proximaPergunta(){
+   montarPergunta() 
+   adicionarEventoInputs()
+}
+
+function adicionarEventoInputs(){
+    const inputsResposta = document.querySelectorAll(".alternativas input")
+    inputsResposta.forEach(input => {
+        input.addEventListener("click", guardarResposta)
+
+        if (input.value === quiz.questions[pergunta-1].answer){
+            respostaCorretaId = input.id
+        }
+    })
+}
+
